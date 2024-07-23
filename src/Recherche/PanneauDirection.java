3888f204653eca41f554c1527ea9ef128db0ae82
dion13129 <dion1312@icloud.com>
2024-07-23 19:48:12 -0400

    Update PanneauDirection.java

--- src/Recherche/PanneauDirection.java
+++ src/Recherche/PanneauDirection.java
@@ -6,25 +6,56 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.*;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer </p>
+ *
+ * <p>Classe	: GUI.PanneauDirection</p>
+ *
+ * <p>Desc		: Panneau (JPanel) dans lequel se trouve nos options de direction.</p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 14/07/24
+ */
 public class PanneauDirection extends JPanel{
 	
+	//Le titre du panneau
 	private JLabel titre;
 	
-	private JCheckBox caseAvant;
-	private JCheckBox caseArriere;
+	//L'option d'aller vers l'avant
+	private JRadioButton caseAvant;
 	
-	public PanneauDirection() {
+	//L'option d'aller vers l'arrière
+	private JRadioButton caseArriere;
+	
+	//Le panneau principal dans lequel ce panneau est ajouté
+	private JPanel panneauPrincipal;
+	
+	/**
+	 * Construit le panneau de direction où les options "avant" et 
+	 * "arrière" (JRadioButton) s'y retrouvent.
+	 *
+	 * @param panneauPrincipal le panneau principal où on ajoute ce panneau.
+	 */
+	public PanneauDirection(JPanel panneauPrincipal) {
 		
-		// met box layout pour que les panneaux imbriques soient les uns par
+		//assigne le panneauPrincipal recu en parametre
+		this.panneauPrincipal = panneauPrincipal;
+		
+		// met box layout pour que les composants imbriqués soient les uns par
 		// dessus les autres
-		//setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-		//setLayout(new FlowLayout());
-		setLayout(new GridLayout(3,1));
-		setPreferredSize(new Dimension(300, 1));
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+				
 		initComposants();
 		
 	}
 	
+	/**
+	 * Initialise tous les composants du panneau de direction en appelant leurs
+	 * fonctions d'initialisation individuelles.
+	 */
 	private void initComposants() {
 		
 		creerEtiq();
@@ -35,12 +66,21 @@ public class PanneauDirection extends JPanel{
 		ajouterComposants();
 	}
 	
+	/**
+	 * Créé l'étiquette qui comporte le titre du panneau
+	 */
 	private void creerEtiq() {
 		
 		titre = nouvelleEtiq(titre);
-		//titre.setAlignmentX(Component.LEFT_ALIGNMENT);
 	}
 	
+	/**
+	 * Construit une nouvelle étiquette et l'assigne à celle reçue en paramètre
+	 *
+	 * @param etiq l'étiquette à laquelle l'étiquette crée sera assignée.
+	 * 
+	 * @return un JLabel associé à celui reçu en paramètre.
+	 */
 	private JLabel nouvelleEtiq(JLabel etiq) {
 		
 		etiq = new JLabel();
@@ -48,22 +88,32 @@ public class PanneauDirection extends JPanel{
 		return etiq;
 	}
 	
+	/**
+	 * Créé les deux cases à cocher qui représenteront les options.
+	 */
 	private void creerCase() {
 		
 		caseAvant = nouvelleCase(caseAvant);
 		caseArriere = nouvelleCase(caseArriere);
-
-		//caseArriere.setAlignmentX(Component.LEFT_ALIGNMENT);
-		//caseAvant.setAlignmentX(Component.LEFT_ALIGNMENT);
 	}
 	
-	private JCheckBox nouvelleCase(JCheckBox caseACocher) {
+	/**
+	 * Construit une nouvelle case et l'assigne à celle reçue en paramètre
+	 *
+	 * @param caseACocher la case à laquelle la case crée sera assignée.
+	 * 
+	 * @return un JRadioButton associé à celui reçu en paramètre.
+	 */
+	private JRadioButton nouvelleCase(JRadioButton caseACocher) {
 		
-		caseACocher = new JCheckBox();
+		caseACocher = new JRadioButton();
 		
 		return caseACocher;
 	}
 	
+	/**
+	 * On ajoute le titre du panneau et celui de chaque case à cocher.
+	 */
 	private void initialiserComposants() {
 		
 		titre.setText("Direction");
@@ -73,6 +123,9 @@ public class PanneauDirection extends JPanel{
 		caseArriere.setText("Arrière");
 	}
 	
+	/**
+	 * On ajoute le titre du panneau et les cases à cocher.
+	 */
 	private void ajouterComposants() {
 		
 		add(titre);
@@ -80,3 +133,4 @@ public class PanneauDirection extends JPanel{
 		add(caseArriere);
 	}
 }
+
