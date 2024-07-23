9313d16adc0a24cb9016e3260f930e8a55b39840
dion13129 <dion1312@icloud.com>
2024-07-23 19:50:52 -0400

    Update PanneauBtn.java

--- src/Recherche/PanneauBtn.java
+++ src/Recherche/PanneauBtn.java
@@ -5,24 +5,61 @@ import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer </p>
+ *
+ * <p>Classe	: GUI.PanneauBtn</p>
+ *
+ * <p>Desc		: Panneau (JPanel) dans lequel se trouve nos boutons.</p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 14/07/24
+ */
 public class PanneauBtn extends JPanel{
 	
+	//Le bouton qui permet de trouver le mot recherché
 	private JButton btnTrouver;
+	
+	//Le bouton qui permet de tout selectionner les mots recherchés
 	private JButton btnSelectionnerTout;
+	
+	//Le bouton qui permet de remplacer le mot recherché par son remplaçant et
+	//de sélectionner le prochain mot 
 	private JButton btnRemplacerTrouver;
+	
+	//Le bouton qui permet de remplacer le mot recherché par son remplaçant
 	private JButton btnRemplacer;
+	
+	//Le bouton qui permet de remplacer toutes les instances du mot recherché 
+	//par son remplaçant
 	private JButton btnRemplacerTout;
 	
-	public PanneauBtn() {
+	//Le bouton qui permet d'accéder à la prochaine instance du mot recherché
+	private JButton btnSuivant;
+	
+	//Le panneau principal dans lequel ce panneau est ajouté
+	private JPanel panneauPrincipal;
+	
+	public PanneauBtn(JPanel panneauPrincipal) {
 		
-		// met box layout pour que les panneaux imbriques soient les uns par
+		//assigne le panneauPrincipal recu en parametre
+		this.panneauPrincipal = panneauPrincipal;
+		
+		// met box layout pour que les composants imbriqués soient les uns par
 		// dessus les autres
-		setLayout(new FlowLayout());
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 				
 		initComposants();
 		
 	}
 	
+	/**
+	 * Initialise tous les composants du panneau de boutons en appelant leurs
+	 * fonctions d'initialisation individuelles.
+	 */
 	private void initComposants() {
 	
 		creerBtn();
@@ -32,6 +69,10 @@ public class PanneauBtn extends JPanel{
 		ajouterComposants();
 	}
 	
+	
+	/**
+	 * Créé les six boutons
+	 */
 	private void creerBtn() {
 		
 		btnTrouver = nouveauBtn(btnTrouver);
@@ -39,17 +80,26 @@ public class PanneauBtn extends JPanel{
 		btnRemplacerTrouver = nouveauBtn(btnRemplacerTrouver);
 		btnRemplacer = nouveauBtn(btnRemplacer);
 		btnRemplacerTout = nouveauBtn(btnRemplacerTout);
+		btnSuivant = nouveauBtn(btnSuivant);
 	}
 	
+	/**
+	 * Construit un nouveau bouton et l'assigne à celui reçu en paramètre
+	 *
+	 * @param btn le bouton auquel le bouton créé sera assigné.
+	 * 
+	 * @return un JButton associé à celui reçu en paramètre.
+	 */
 	private JButton nouveauBtn(JButton btn) {
 		
 		btn = new JButton();
-
-		//btn.setPreferredSize(new Dimension(120, 20));
 		
 		return btn;
 	}
 	
+	/**
+	 * On ajoute un titre à chaque bouton.
+	 */
 	private void initialiserComposants() {
 		
 		btnTrouver.setText("Trouver");
@@ -61,8 +111,13 @@ public class PanneauBtn extends JPanel{
 		btnRemplacer.setText("Remplacer");
 		
 		btnRemplacerTout.setText("Remplacer tout");
+		
+		btnSuivant.setText("Suivant");
 	}
 	
+	/**
+	 * On ajoute les boutons.
+	 */
 	private void ajouterComposants() {
 		
 		add(btnTrouver);
@@ -70,5 +125,6 @@ public class PanneauBtn extends JPanel{
 		add(btnRemplacerTrouver);
 		add(btnRemplacer);
 		add(btnRemplacerTout);
+		add(btnSuivant);
 	}
 }
