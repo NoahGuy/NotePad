7bd693601be52de5c13c1619d561ee9eaaf0baaa
dion13129 <dion1312@icloud.com>
2024-08-07 13:46:46 -0400

    Update BarreEtat.java

--- src/Notepad/BarreEtat.java
+++ src/Notepad/BarreEtat.java
@@ -10,6 +10,23 @@ import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.text.Element;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
+ *
+ * <p>Classe	: </p>
+ *
+ * <p>Desc		: </p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 07/08/24
+ */
+
 public class BarreEtat extends JPanel {
 
     private JLabel zoom;
