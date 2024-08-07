fda3f195e70894eeeeae19133efe05b548b4f40d
dion13129 <dion1312@icloud.com>
2024-08-07 13:45:57 -0400

    queques comentaire

--- src/Recherche/PanneauPrincipal.java
+++ src/Recherche/PanneauPrincipal.java
@@ -9,7 +9,10 @@ import javax.swing.JPanel;
 /**
  * Travail Pratique 2 INF111
  *
- * <p>L'objectif de ce travail pratique est de créer </p>
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
  *
  * <p>Classe	: GUI.PanneauPrincipal</p>
  *
@@ -18,7 +21,7 @@ import javax.swing.JPanel;
  *
  * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
  *
- * @version 14/07/24
+ * @version 07/08/24
  */
 
 public class PanneauPrincipal extends JPanel{
