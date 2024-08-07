e62d212bb38dd0f41fefe78f8214fe44e29775c5
dion13129 <dion1312@icloud.com>
2024-08-07 13:47:26 -0400

    Update DemarrerGUI.java

--- src/Notepad/DemarrerGUI.java
+++ src/Notepad/DemarrerGUI.java
@@ -1,5 +1,21 @@
 package Notepad;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
+ *
+ * <p>Classe	: </p>
+ *
+ * <p>Desc		: </p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 07/08/24
+ */
 
 public class DemarrerGUI {
     public static void main(String[] args) {
