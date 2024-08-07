bab028a602ef268583f2594bfcba2cb81ffca2a7
dion13129 <dion1312@icloud.com>
2024-08-07 13:47:03 -0400

    Update CadreGUI.java

--- src/Notepad/CadreGUI.java
+++ src/Notepad/CadreGUI.java
@@ -6,6 +6,23 @@ import java.awt.*;
 import static javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED;
 import static javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
+ *
+ * <p>Classe	: </p>
+ *
+ * <p>Desc		: </p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 07/08/24
+ */
+
 public class CadreGUI extends JFrame {
     private PanneauPrincipal panneauPrincipal;
     private Fonctions fonctions;
