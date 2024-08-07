cc1eaf58bb4eb597383f4402a1e20f604bf81a0f
dion13129 <dion1312@icloud.com>
2024-08-07 13:14:40 -0400

    ajouts de quelques commentaires

--- src/Recherche/PanneauPrincipal.java
+++ src/Recherche/PanneauPrincipal.java
@@ -47,22 +47,31 @@ public class PanneauPrincipal extends JPanel{
 
 	public void initComposants(Fonctions fonctions, Cadre cadre) {
 
-
+		// met box layout pour que les panneaux imbriqués soient les uns par
+		// dessus les autres.
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
-
+		// construit le panneau où on insère notre mot à remplacer et son
+		// remplacement.
 		panneauMots = new PanneauMots();
 
+		
 		panneauDirection = new PanneauDirectionOptions(this);
 
-
+		// construit le panneau où on insère nos boutons et utilisation de paramètres
+		// pour qu'on puisse interagir entre nos classes de panneaux et avec nos
+		// méthodes de la classe fonctions.
 		panneauBtn = new PanneauBtn(fonctions, panneauMots, panneauDirection);
 
+		// construit le panneau où on insère notre boutons qui ferme la
+		// fenêtre et lui passe le panneau principal et le cadre en paramètre 
+		// afin qu'il puisse interagir ensemble.
 		panneauFermer = new PanneauFermer(this, cadre);
 	}
 
 	public void ajouterComposants() {
 
+		// ajoute tous les panneaux créés au panneau principal
 		add(panneauMots);
 		add(panneauDirection);
 		add(panneauBtn);
