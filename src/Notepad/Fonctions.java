9df72f42b9ea9b4e53bb7836d79fdfd5de5a55d9
dion13129 <dion1312@icloud.com>
2024-08-07 13:47:35 -0400

    Update Fonctions.java

--- src/Notepad/Fonctions.java
+++ src/Notepad/Fonctions.java
@@ -13,6 +13,23 @@ import javax.swing.text.StyledDocument;
 
 import static java.awt.FileDialog.SAVE;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
+ *
+ * <p>Classe	: </p>
+ *
+ * <p>Desc		: </p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 07/08/24
+ */
+
 public class Fonctions {
     private static final int MAX_FONT_SIZE = 60;
     private static final int MIN_FONT_SIZE = 2;
