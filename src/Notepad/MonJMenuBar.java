ec9d87b37c5d1a7a9c04d927facf3dc56303579b
dion13129 <dion1312@icloud.com>
2024-08-07 13:47:54 -0400

    Update MonJMenuBar.java

--- src/Notepad/MonJMenuBar.java
+++ src/Notepad/MonJMenuBar.java
@@ -7,6 +7,23 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
+/**
+ * Travail Pratique 2 INF111
+ *
+ * <p>L'objectif de ce travail pratique est de créer un éditeur de texte simple avec les 
+ *    options ‘défaire’ (undo), via Ctrl+z, et ‘refaire’ (redo), via ‘Ctrl+y’. L'éditeur
+ *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
+ *    la commande ‘Ctrl+f’.</p>
+ *
+ * <p>Classe	: </p>
+ *
+ * <p>Desc		: </p>
+ *
+ * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
+ *
+ * @version 07/08/24
+ */
+
 public class MonJMenuBar extends JMenuBar implements ActionListener {
     private JMenu menuFichier;
     private JMenu menuModifier;
