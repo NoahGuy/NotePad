61a0eb172109c016319b36ac68556737048ce4d3
S3277 <souhayl.farsane.1@ens.etsmtl.ca>
2024-08-07 22:47:16 -0400

    Commentaire MonJmenuBar

--- src/Notepad/MonJMenuBar.java
+++ src/Notepad/MonJMenuBar.java
@@ -15,18 +15,22 @@ import javax.swing.JMenuItem;
  *    a aussi une fonctionnalité de recherche et de remplacement de mots qu'on accède via
  *    la commande ‘Ctrl+f’.</p>
  *
- * <p>Classe	: </p>
+ * <p>Classe	: MonJMenuBar</p>
  *
- * <p>Desc		: </p>
+ * <p>Desc		: Cette classe crée la barre de menu pour l'éditeur de texte avec des options
+ *                de fichier et d'affichage, et gère les actions associées aux éléments de menu.</p>
  *
  * @author Josue Jesus Aliaga Guillen, Noah Boivin, Simon Dion, Souhayl Farsane
  *
  * @version 07/08/24
  */
-
 public class MonJMenuBar extends JMenuBar implements ActionListener {
+
+    //Les options qui seront dans le menu
     private JMenu menuFichier;
     private JMenu menuAfficher;
+
+    //Les items presents dans les options
     private JMenuItem itemNouveau;
     private JMenuItem itemOuvrir;
     private JMenuItem itemSauvegarder;
@@ -36,25 +40,28 @@ public class MonJMenuBar extends JMenuBar implements ActionListener {
     private JMenuItem itemZoomIn;
     private JMenuItem itemZoomOut;
 
+
     private Fonctions fonctions;
 
+    // Constructeur qui initialise la barre de menu avec les fonctions données
     public MonJMenuBar(Fonctions fonctions) {
-
         this.fonctions = fonctions;
         this.creerBarMenu();
         this.creerFichierMenu();
     }
 
+    // Crée la barre de menu
     private void creerBarMenu() {
         this.ajouterMenu();
     }
 
+    // Crée les éléments de menu de fichier
     private void creerFichierMenu() {
         this.initcomposant();
     }
 
+    // Ajoute les menus Fichier et Affichage à la barre de menu
     private void ajouterMenu() {
-
         menuFichier = new JMenu("Fichier");
         add(this.menuFichier);
 
@@ -62,85 +69,63 @@ public class MonJMenuBar extends JMenuBar implements ActionListener {
         add(this.menuAfficher);
     }
 
+    // Initialise les composants de menu et les ajoute aux menus appropriés
     private void initcomposant() {
 
-        itemNouveau = nouveauMenuItem("Nouveau", menuFichier);
+        itemNouveau =   nouveauMenuItem("Nouveau", menuFichier);
+        itemOuvrir  =   nouveauMenuItem("Ouvrir", menuFichier);
 
-        itemOuvrir = nouveauMenuItem("Ouvrir", menuFichier);
-
-        itemSauvegarder = nouveauMenuItem("Sauvegarder", menuFichier);
-
-        itemSaveAs = nouveauMenuItem("Sauvegarder sous", menuFichier);
-
-        itemQuitter = nouveauMenuItem("Quitter", menuFichier);
+        itemSauvegarder = nouveauMenuItem("Sauvegarder",       menuFichier);
+        itemSaveAs      = nouveauMenuItem("Sauvegarder sous",  menuFichier);
+        itemQuitter     = nouveauMenuItem("Quitter",           menuFichier);
 
         itemBarreEtat = nouveauMenuItem("Barre état", menuAfficher);
-
-        itemZoomIn = nouveauMenuItem("Zoom in", menuAfficher);
-
-        itemZoomOut = nouveauMenuItem("Zoom out", menuAfficher);
+        itemZoomIn    = nouveauMenuItem("Zoom in",    menuAfficher);
+        itemZoomOut   = nouveauMenuItem("Zoom out",   menuAfficher);
     }
 
+    // Crée un nouvel élément de menu avec le nom donné et l'ajoute au menu spécifié
     public JMenuItem nouveauMenuItem(String nom, JMenu menu) {
 
         JMenuItem item = new JMenuItem(nom);
+
         item.addActionListener(this);
         item.setActionCommand(nom);
+        
         menu.add(item);
         return item;
     }
 
+    // Gère les actions effectuées sur les éléments de menu
     public void actionPerformed(ActionEvent e) {
+
         switch (e.getActionCommand()) {
-
             case "Nouveau":
-
                 fonctions.nouveauFichier();
                 break;
-
-
             case "Ouvrir":
-
                 fonctions.ouvrirFichier();
                 break;
-
-
             case "Sauvegarder":
-
                 fonctions.save();
                 break;
-
-
-            case "Sauvegarder en tant que":
-
+            case "Sauvegarder sous":
                 fonctions.saveAs();
                 break;
-
-
             case "Quitter":
-
                 fonctions.quitter();
                 break;
-
-
             case "Zoom in":
-
                 fonctions.zoomIn();
                 this.menuAfficher.doClick();
                 break;
-
-
             case "Zoom out":
-
                 fonctions.zoomOut();
                 this.menuAfficher.doClick();
                 break;
-
-
             case "Barre état":
-
                 fonctions.enleverRemettreBarreEtat();
+                break;
         }
-
     }
 }
